cede2c33bfee4903d5bcfcb58dca38a32a49bab0
Avila Diego <aviladiego2009@gmail.com>
2025-05-24 10:52:19 -0300

    Added startup module

---
+++ src/startup.rs
@@ -0,0 +1,9 @@
+use actix_web::dev::Server;
+use actix_web::{App, HttpServer};
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new()).listen(listener)?.run();
+
+    Ok(server)
+}
